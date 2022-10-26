01fb4b78ca2ddb27bf117cc0febe6ec2482c2e68
LabUser <pkell19@wgu.edu>
2022-10-25 21:02:23 -0600

    Added models and customer fxml

--- src/controller/NewCustomer.java
+++ src/controller/NewCustomer.java
@@ -1,8 +1,30 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 public class NewCustomer {
+    public TextField newCustomerName;
+    public TextField newAddress;
+    public TextField newPostalCode;
+    public TextField newPhoneNumber;
+    public TextField newCountry;
+    public TextField newDivision;
+    public TextField newCustomerID;
+    public TextField apptSearch;
+
     public void onActionToCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onActionToReport(ActionEvent actionEvent) {
+    }
+
+    public void onActionExit(ActionEvent actionEvent) {
+    }
+
+    public void onActSaveCustList(ActionEvent actionEvent) {
+    }
+
+    public void onActNoSaveCustList(ActionEvent actionEvent) {
     }
 }
--- src/controller/UpdateCustomer.java
+++ src/controller/UpdateCustomer.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class UpdateCustomer {
+    public TextField updateCustomerName;
+    public TextField updateAddress;
+    public TextField updatePostalCode;
+    public TextField updatePhoneNumber;
+    public TextField updateCountry;
+    public TextField updateDivision;
+    public TextField customerID;
+    public TextField apptSearch;
+
+    public void onActSaveCustList(ActionEvent actionEvent) {
+    }
+
+    public void onActNoSaveCustList(ActionEvent actionEvent) {
+    }
+
+    public void onActionToCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onActionToReport(ActionEvent actionEvent) {
+    }
+
+    public void onActionExit(ActionEvent actionEvent) {
+    }
 }
